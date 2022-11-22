601d767ad49c51aab31b9507044802c2472f2f1b
TomisTony <1158176767@qq.com>
2022-11-22 20:40:26 +0800

    布局完成

--- src/MiniCAD.java
+++ src/MiniCAD.java
@@ -1,31 +1,92 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MiniCAD {
-
-    public static void main(String[] args) {
-        final JFrame jf = new JFrame("测试窗口");
-        jf.setSize(400, 400);
-        jf.setLocationRelativeTo(null);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        JPanel panel = new JPanel();
-
-        JButton btn = new JButton("Show New Window");
-        btn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // 点击按钮, 显示新的一个窗口
-                showNewWindow(jf);
-            }
-        });
-        panel.add(btn);
-
-        jf.setContentPane(panel);
-        jf.setVisible(true);
+    final JFrame mainJFrame = new JFrame("MiniCAD");
+    JPanel drawBoard = new JPanel();
+    JPanel toolBar = new JPanel();
+    JPanel bottomBar = new JPanel();
+    MiniCAD(){
+        setMainJFrame();
+        setToolBar();
+        setDrawBoard();
+        setBottomBar();
+        mainJFrame.setVisible(true);
     }
+    private void setMainJFrame(){
+        mainJFrame.setSize(defaultSettings.WINDOW_WIDTH,defaultSettings.WINDOW_HEIGHT);
+        mainJFrame.setResizable(false);//不允许改变大小，以免出现排版问题
+        mainJFrame.setLocationRelativeTo(null);
+        mainJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainJFrame.add(drawBoard, BorderLayout.CENTER);
+        mainJFrame.add(toolBar, BorderLayout.WEST);
+        mainJFrame.add(bottomBar, BorderLayout.SOUTH);
+    }
+    private void setToolBar(){
+        toolBar.setPreferredSize(new Dimension(defaultSettings.TOOLBAR_WIDTH, defaultSettings.TOOLBAR_HEIGHT));
+        toolBar.setBackground(Color.LIGHT_GRAY);
+        setToolButtons();
+        setColorButtons();
+    }
+    private void setToolButtons(){
+        String[] shapeName = { "choose", "rectangular", "circle", "line", "text", "delete", "open", "save" };
+        for (int i = 0; i < shapeName.length; i++) {
+            JButton toolButton = new JButton(shapeName[i]);
+            toolButton.setIcon(getFixedImageIcon("src/icons/" + shapeName[i] + ".png",shapeName[i],
+                    defaultSettings.TOOL_ICON_SIDE_LENGTH,defaultSettings.TOOL_ICON_SIDE_LENGTH));
+            toolButton.setPreferredSize(new Dimension(defaultSettings.TOOL_ICON_SIDE_LENGTH+10,
+                    defaultSettings.TOOL_ICON_SIDE_LENGTH+10));
+            toolButton.setBackground(Color.white);
+
+//            toolButton.addActionListener(myMouse);
+            toolBar.add(toolButton);
+        }
+    }
+    private void setColorButtons(){
+        Color[] colors = {Color.red, Color.pink, Color.orange, Color.yellow, Color.green, Color.blue, Color.cyan,
+                Color.black, Color.gray, Color.white};
+        for (int i = 0; i < colors.length; i++) {
+            JButton colorButton = new JButton();
+            colorButton.setBackground(colors[i]);
+            colorButton.setPreferredSize(new Dimension(defaultSettings.COLOR_SIDE_LENGTH ,
+                    defaultSettings.COLOR_SIDE_LENGTH));
+//            nowButton.addActionListener(myMouse);
+            toolBar.add(colorButton);
+        }
+    }
+    private void setDrawBoard(){}
+    private void setBottomBar(){
+        bottomBar.setPreferredSize(new Dimension(defaultSettings.BOTTOMBAR_WIDTH,defaultSettings.BOTTOMBAR_HEIGHT));
+        bottomBar.setBackground(Color.white);
+        JLabel notifyLable = new JLabel(defaultSettings.NOTIFY_IDLE);
+        notifyLable.setForeground(Color.black);
+        notifyLable.setVisible(true);
+        notifyLable.setBackground(Color.BLUE);
+        notifyLable.setHorizontalAlignment(SwingConstants.LEFT);
+        notifyLable.setVerticalAlignment(SwingConstants.TOP);
+        bottomBar.add(notifyLable);
+    }
+
+    /**
+     * @description: 导入image并返回重置大小的ImageIcon
+     * @param imageFileName: 导入的imageFile的路径
+     * @param description: ImageIcon自带的Description
+     * @param height: 重置的高度
+     * @param width: 重置的宽度
+     * @return javax.swing.ImageIcon
+     */
+    private ImageIcon getFixedImageIcon(String imageFileName, String description, int height, int width){
+        ImageIcon icon = new ImageIcon(imageFileName,description);
+        Image img = icon.getImage();
+        img = img.getScaledInstance(width,height,Image.SCALE_DEFAULT);
+        icon.setImage(img);
+        return icon;
+    }
+    public static void main(String[] args) {
+        new MiniCAD();
+    }
+
+
 
     public static void showNewWindow(JFrame relativeWindow) {
         // 创建一个新窗口
---
+++ src/defaultSettings.java
@@ -0,0 +1,13 @@
+public class defaultSettings {
+    public static final int WINDOW_WIDTH = 1200;//窗口宽度
+    public static final int WINDOW_HEIGHT = 815;//窗口高度
+    public static final int TOOLBAR_WIDTH = 90;//工具栏宽度
+    public static final int BOTTOMBAR_WIDTH = WINDOW_WIDTH - TOOLBAR_WIDTH;//底部栏宽度
+    public static final int BOTTOMBAR_HEIGHT = 30;//底部栏高度
+    public static final int TOOLBAR_HEIGHT = WINDOW_HEIGHT - BOTTOMBAR_HEIGHT;//工具栏高度
+    public static final int TOOL_ICON_SIDE_LENGTH = 50;//工具图标边长
+    public static final int COLOR_SIDE_LENGTH = 40;//颜色选择器边长
+
+    public static final String NOTIFY_IDLE = "(idle)";
+
+}
